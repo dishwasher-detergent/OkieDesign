dda763051bc703478d5fade461e12d58b081385f
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2022-02-08 08:15:50 -0600

    Update _app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -14,25 +14,22 @@ function MyApp({ Component, pageProps }) {
         <meta name="msapplication-TileColor" content="#2d89ef" />
         <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
-//      Meta tags for Social Media
-//      Essential META Tags
+
         <meta property="og:title" content="Okie Design - CSS components" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/favicon/apple-touch-icon.png" />
         <meta property="og:url" content="https://www.okie.design" />
         <meta name="twitter:card" content="summary_large_image" />
 
-//      Non-Essential, But Recommended
         <meta property="og:description" content="Responsive CSS components made with TailwindCSS and Love." />
         <meta property="og:site_name" content="Okie Design" />
         <meta name="twitter:image:alt" content="Okie Deisng - CSS Components" />
     
-//      Meta tags for SEO
         <meta name="title" content="Okie Design" />
         <meta name="description" content="Responsive CSS components made with TailwindCSS and Love." />
         <meta name="keywords" content="tailwind,css,framework,UI,UX,design,web design,websites,css components,components,ui-library,tailwindcss,design-system" />
         <meta name="robots" content="index, follow" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="English" />
         <meta name="revisit-after" content="7 days" />
         <meta name="author" content="Kenneth Bass" />
