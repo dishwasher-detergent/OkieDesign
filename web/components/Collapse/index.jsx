b4a031015a0163d5a566313f2541390529f760e9
dishwasher-detergent <kenny_bass@outlook.com>
2022-02-14 19:39:18 -0600

    Fix Links

--- web/components/Collapse/index.jsx
+++ web/components/Collapse/index.jsx
@@ -9,7 +9,7 @@ const Collapse = () => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-            These examples utilize <a className='button sm' href="https://headlessui.dev/react/disclosure">HeadlessUI</a>.
+            These examples utilize <Link target="_blank" href="https://headlessui.dev/react/disclosure"><a className='button sm'>HeadlessUI</a></Link>.
         </div>
         <ComponentDocs classes="list" title="Sizes">
           <div className='collapse xs w-60'>
--- web/components/Dropdown/index.jsx
+++ web/components/Dropdown/index.jsx
@@ -9,7 +9,7 @@ const Dropdown = () => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-            These examples utilize <a className='button sm' target="_blank" href="https://headlessui.dev/react/menu">HeadlessUI</a>.
+            These examples utilize <Link target="_blank" href="https://headlessui.dev/react/menu"><a className='button sm'>HeadlessUI</a></Link>.
         </div>
         <ComponentDocs classes="list" title="Sizes">
             <Menu as="div" className="dropdown left">
